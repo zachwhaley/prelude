eff643bfce2b0f513ac75ad9d86ed5569b452243
Zach Whaley <zachbwhaley@gmail.com>
2014-02-28 17:18:38 -0600

    Added lots of things
    Vim Tab and BackTab work like I think they should.
    Set line length to 120.
    show line numbers.
    Use dirtree for directory tree of files.
    use projectil caching.
    And I've been introduced to ORG mode.

--- personal/personal.el
+++ personal/personal.el
@@ -1,7 +1,25 @@
-(evil-mode 1)
-
 ;; Vim key mappings
+(evil-mode 1)
+;; Hold Ctrl while in insert mode to change "hjkl" back to directional keys.
 (define-key evil-insert-state-map (kbd "C-h") 'backward-char)
 (define-key evil-insert-state-map (kbd "C-l") 'forward-char)
 (define-key evil-insert-state-map (kbd "C-j") 'next-line)
 (define-key evil-insert-state-map (kbd "C-k") 'previous-line)
+(define-key evil-insert-state-map "\t" 'evil-shift-right-line)
+(define-key evil-insert-state-map [backtab] 'evil-shift-left-line)
+
+;;Line length to 120
+(setq whitespace-line-column 120) ;; limit line length
+
+;; Show line numbers
+(require 'nlinum)
+(global-nlinum-mode 1)
+
+;; Show all the files in current directory
+(require 'dirtree)
+
+;; Enable Projectile caching to prevent additional reindexing
+(setq project-enable-caching t)
+
+;; Make org mode look nicer
+(setq org-startup-indented t)
