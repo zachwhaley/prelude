5f5b963fff4bc7487d62c6b913de600fc38f7a99
Zach Whaley <zachbwhaley@gmail.com>
2014-02-28 13:50:38 -0600

    Added Vim key mappings
    While in insert mode, hold down Ctrl to change the "hjkl" row back to directional keys.

--- personal/personal.el
+++ personal/personal.el
@@ -1,1 +1,7 @@
 (evil-mode 1)
+
+;; Vim key mappings
+(define-key evil-insert-state-map (kbd "C-h") 'backward-char)
+(define-key evil-insert-state-map (kbd "C-l") 'forward-char)
+(define-key evil-insert-state-map (kbd "C-j") 'next-line)
+(define-key evil-insert-state-map (kbd "C-k") 'previous-line)
