45a85086a3e25cb08d3ac9209bc39f8fdff12a39
Zach Whaley <zachbwhaley@gmail.com>
2014-03-05 13:39:04 -0600

    Being more specific with programming languages
    Only programming languages that use curly braces should hook into the
    new line curly brace function.

--- personal/programming.el
+++ personal/programming.el
@@ -3,6 +3,11 @@
 ;;; Code:
 
 (defun new-line-dwim ()
+  "Make smartparens-mode better.
+Pressing return after creating an opening and closing paren via
+smartparens-mode will place your cursor one line down and indent it while
+placing the closing paren one more line down and at the same column as it's
+statement."
   (interactive)
   (let ((break-open-pair (or (and (looking-back "{") (looking-at "}"))
                              (and (looking-back ">") (looking-at "<"))
@@ -15,9 +20,12 @@
         (indent-for-tab-command)))
     (indent-for-tab-command)))
 
-(defun add-keys ()
+(defun programming-keys ()
+  "Add custom key mappings when editing programming languages."
   (local-set-key (kbd "RET") 'new-line-dwim))
-(add-hook 'prog-mode-hook 'add-keys)
+
+(add-hook 'c-mode-hook 'programming-keys)
+(add-hook 'java-mode-hook 'programming-keys)
 
 (provide 'programming)
 ;;; programming.el ends here
