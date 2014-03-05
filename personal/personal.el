e3fbb7800b85b1ff534cc391282cb3510dda2309
Zach Whaley <zachbwhaley@gmail.com>
2014-03-04 21:20:40 -0600

    Set custom ujelly theme as default theme

--- personal/personal.el
+++ personal/personal.el
@@ -36,5 +36,9 @@
 (global-set-key (kbd "C-S-o") 'split-window-below)
 (global-set-key (kbd "C-S-e") 'split-window-right)
 
+;; Set custom ujelly theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/ujelly-theme")
+(load-theme 'ujelly t)
+
 (provide 'personal)
 ;;; personal.el ends here
