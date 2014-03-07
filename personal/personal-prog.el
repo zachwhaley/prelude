a28a265ddd23edb37157bb25f514d478c3626702
Zach Whaley <zachbwhaley@gmail.com>
2014-03-07 11:40:52 -0600

    Edited personal-prog yasnippet loading.
    Moved (yas-minor-mode) to personal-prog-mode-hook.
    Trying to keep things clean.  Also still learning about elisp.

--- personal/personal-prog.el
+++ personal/personal-prog.el
@@ -4,14 +4,13 @@
 
 ;; YASnippet magic!
 (require 'yasnippet)
-
 (yas-reload-all)
-(add-hook 'prelude-prog-mode-hook '(lambda () (yas-minor-mode)) t)
 
 (defun personal-prog-mode-defaults ()
   "Personal default coding hook."
   ;; For God's sake indent when I press return.
-  (electric-indent-mode +1))
+  (electric-indent-mode +1)
+  (yas-minor-mode))
 
 (add-hook 'prelude-prog-mode-hook 'personal-prog-mode-defaults t)
 
