4b3b4a920cb9578a4cdfb61678c0bd681841bf6c
Zach Whaley <zachbwhaley@gmail.com>
2014-03-18 09:17:45 -0500

    Fixed solarized colors for Python code
    Python was giving the wrong font faces for the solarized theme.
    This website had the fix http://www.ummae.com/2013/01/06/emacs-with-solarized-dark-on-iterm2-python

--- personal/personal-python.el
+++ personal/personal-python.el
@@ -3,9 +3,19 @@
 ;;; Code:
 
 (require 'jedi)
+(require 'rx)
 
 (defun personal-python-mode-defaults ()
   "Personal default python hook."
+  ;; Fixing font-lock face for solarized
+  ;; found here: http://www.ummae.com/2013/01/06/emacs-with-solarized-dark-on-iterm2-python/
+  (font-lock-add-keywords nil
+                          '(("\\<\\([0-9]+\\(eE[+-]?[0-9]*\\)?\\|0[xX][0-9a-fA-F]+\\)\\>" . font-lock-constant-face)))
+  (font-lock-add-keywords nil `(( ,(rx symbol-start (or "print" "exit" ) symbol-end) . font-lock-function-name-face)))
+  (font-lock-add-keywords nil `(( ,(rx symbol-start (or "import" "from" ) symbol-end) . font-lock-preprocessor-face)))
+  (font-lock-add-keywords nil `(( ,(rx "\\" (0+ blank) line-end ) . font-lock-preprocessor-face)))
+  (font-lock-add-keywords nil '(( "\\(%[sdf]\\)" 1 font-lock-preprocessor-face prepend)))
+  (font-lock-add-keywords nil '(( "\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face prepend)))
   (jedi:setup)
   (jedi:ac-setup))
 
